3eac6516457c62d037ed18bec0b1160343d1aa26
lince098 <pablogamarro@hotmail.com>
2023-07-08 11:14:46 +0200

    Added Automated Responses

--- challenge_functions/answer_reviews.py
+++ challenge_functions/answer_reviews.py
@@ -1,18 +1,76 @@
 from qdrant_client import QdrantClient
+
+from qdrant_client.http.models import SearchRequest
 from sentence_transformers import SentenceTransformer
 import streamlit as st
-import asyncio
+import os
+from dotenv import load_dotenv
+import logging
+from itertools import chain
 
-@st.cache_resource  
+
+load_dotenv(override=False)
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+COLLECTION_NAME = "restaurant_review_answers"
+
+
+@st.cache_resource
+def get_qdrant_connection():
+    host = os.environ["QDRANT_HOST"]
+    port = os.environ["QDRANT_PORT"]
+    return QdrantClient(host, port=port)
+
+
+@st.cache_resource
 def get_embedding_model():
     return SentenceTransformer("all-MiniLM-L6-v2")
 
-@st.cache_resource
-def get_qdrant_client():
-    QdrantClient("localhost", port=6333)
 
-def search(df):
+def search(selected_list, df):
+    if not selected_list:
+        return
+
+    qdrant = get_qdrant_connection()
     model = get_embedding_model()
-    text_column = df["body"].to_list()
-    embeddings = model.encode(text_column)
-    
+
+    # Take text
+    selected_rows = df.iloc[selected_list, :]
+    selected_rows_list = selected_rows["body"].to_list()
+
+    # Transform text
+    embeddings = model.encode(selected_rows_list)
+
+    logger.debug(f"embeddings length: {len(embeddings)}")
+
+    # Prepare and send requests
+    requests = [
+        SearchRequest(vector=vector.tolist(), limit=1, with_payload=True)
+        for vector in embeddings
+    ]
+
+    hits_not_flattened = qdrant.search_batch(
+        collection_name=COLLECTION_NAME, requests=requests,
+    )
+
+    hits = list(chain.from_iterable(hits_not_flattened))
+
+    if logger.level == logging.DEBUG:
+        logger.debug("PRINTING HITS")
+        for i, hit in enumerate(hits, 1):
+            logger.debug(f"hit {i}: {hit}")
+
+    result = [
+        {"Message": text, "Score": hit.score, "Answer": hit.payload["answer"]}
+        for text, hit in zip(selected_rows_list, hits)
+    ]
+
+    if logger.level == logging.DEBUG:
+        logger.debug("PRINTING Result")
+        for i, item in enumerate(result, 1):
+            print("que paza")
+            logger.debug(f"result {i}: {item}")
+
+    return result
--- pages/Automated_Responses.py
+++ pages/Automated_Responses.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from challenge_functions import answer_reviews as ar, general as g
+import pandas as pd
+import datetime
 
 st.sidebar.markdown("# Automated Responses")
 
@@ -12,4 +14,25 @@ uploaded_file = st.file_uploader("Insert a csv file with TripAdvisor reviews.")
 
 if uploaded_file:
     df = g.load_csv_from_file_input(uploaded_file)
-    st.dataframe(df)
+    with st.form("my_form"):
+        st.markdown("### You can select the rows you want to analyze")
+
+        st.dataframe(df)
+
+        selected_list = st.multiselect("Index", df.index.to_list())
+        submitted = st.form_submit_button("Get Answers")
+
+    if submitted:
+        preds = ar.search(selected_list, df)
+        if preds:
+            preds_df = pd.DataFrame(preds, columns=["Message", "Score", "Answer"])
+            st.dataframe(preds_df)
+
+            fname = f"Sentiment Analysis - {datetime.datetime.now().isoformat()}.csv"
+            st.download_button(
+                label="Download data as CSV",
+                data=preds_df.to_csv(index=False).encode(),
+                file_name=fname,
+            )
+        else:
+            st.markdown(":red[No rows selected.]")
